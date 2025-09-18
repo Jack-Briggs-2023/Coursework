537610f97581dfaa37aca28f427e7f1f76ac4828
Jack Briggs <jackwbriggs@gmail.com>
2025-09-17 22:40:30 -0600

    First commit

---
+++ mainProgram.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() { // Main function
+    cout << "Welcome To Coursework!" << endl;
+    cout << "This program allows you to ensure your classes, and their deadlines are all organized." << endl;
+
+
+
+
+
+
+
+    return 0;
+}
+
